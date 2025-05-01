70e028afce0163e11aaac9eb85bf89946a33e137
lumendesp <mlmsp@cin.ufpe.br>
2025-05-01 18:29:26 -0300

    done

---
+++ src/components/ThanksForm.jsx
@@ -0,0 +1,36 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./ThanksForm.css";
+
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfied: <BsFillEmojiHeartEyesFill />,
+}
+
+const ThanksForm = ({ data }) => {
+  return (
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>A sua opinião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra.</p>
+      <p>Para concluir sua avaliação, clique no botão de Enviar abaixo.</p>
+      <h3>Aqui está um resumo da sua avaliação, {data.name}:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+        {emojiData[data.review]}
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+        {data.comment}
+      </p>
+    </div>
+  )
+}
+
+export default ThanksForm
